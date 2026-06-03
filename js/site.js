70acc389299892ebf3ddf3315e0f671828971298
Rampathak12 <162395208+Rampathak12@users.noreply.github.com>
2026-06-04 00:12:32 +0900

    Load page language translations after site script

--- js/site.js
+++ js/site.js
@@ -7,5 +7,7 @@
     document.body.appendChild(script);
   }
 
-  loadScript('/api-config.js', () => loadScript('/js/site-core.js'));
+  loadScript('/api-config.js', () => {
+    loadScript('/js/site-core.js', () => loadScript('/js/language-pages.js'));
+  });
 })();
